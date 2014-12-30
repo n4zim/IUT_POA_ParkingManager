195427c9bdeff6c7d2a770ce30ba38cb40403e87
Josua Gonzalez <jdpepi@gmail.com>
2014-12-30 15:21:09 +0100

    Parking avec des places qui à des places dedans

--- src/parking/business/Parking.java
+++ src/parking/business/Parking.java
@@ -1,10 +1,35 @@
 package parking.business;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Parking {
+	Collection<Place> places;
+
+	public Parking() {
+		places = new ArrayList<Place>();
+	}
+	
+	public void ajouterPlace(Place place)  {
+		places.add(place);
+	}
+	
+	public void creerPlaces() {
+		for (int i = 0; i < Constante.NOMBRE_PLACES; i++) {
+			if(Math.random() < 0.5) ajouterPlace(new Particulier());
+			else ajouterPlace(new Transporteur());
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return "Parking [places=" + places + "]";
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Parking p = new Parking();
+		p.creerPlaces();
+		System.out.println(p);
 	}
 
 }
