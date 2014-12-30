678405a1b4643cc993c73f5b3669fd251e2a08fd
Josua Gonzalez <jdpepi@gmail.com>
2014-12-30 16:03:07 +0100

    Méthode park(Vehicule), park(Vehicule, Integer) et unpark(Integer) (question 3 et 4)

--- src/parking/business/Parking.java
+++ src/parking/business/Parking.java
@@ -21,12 +21,57 @@ public class Parking {
 		}
 	}
 	
+	private Place getFirstFreePlaceTransporteur() {
+		for (Place p : places.values())
+			if(p instanceof Transporteur)
+				return p;
+		
+		return null;
+	}	
+
+	private Place getFirstFreePlace() {
+		for (Place p : places.values())
+			if(p instanceof Particulier)
+				return p;
+		
+		return getFirstFreePlaceTransporteur();
+	}
+	
+	
+	public void park(Vehicule vehicule) throws PlaceOccupeeException {
+		Place place;
+		
+		if(vehicule instanceof Camion) {
+			place = getFirstFreePlaceTransporteur();
+		} else {
+			place = getFirstFreePlace();
+		}
+		
+		if(place == null) throw new PlaceOccupeeException();
+		
+		place.parkVehicule(vehicule);
+	}
+	
+	public void park(Vehicule vehicule, Integer numeroPlace) throws PlaceOccupeeException {
+		Place place = places.get(numeroPlace);
+		
+		if(place instanceof Particulier && vehicule instanceof Camion)
+			throw new PlaceOccupeeException();
+		
+		place.parkVehicule(vehicule);
+	}
+	
+	public Vehicule unpark(Integer numeroPlace) throws PlaceLibreException { 
+		Place place = places.get(numeroPlace);
+		return place.unparkVehicule();
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Parking [places=" + places + "]";
 	}
 	
-
 	public static void main(String[] args) {
 		Parking p = new Parking();
 		p.creerPlaces();
