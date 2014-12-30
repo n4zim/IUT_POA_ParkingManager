4b83ec08f1bc61aaf3a7ead46f493323d1f570d6
Josua Gonzalez <jdpepi@gmail.com>
2014-12-30 16:22:54 +0100

    Attributs et tostring

--- src/parking/business/Vehicule.java
+++ src/parking/business/Vehicule.java
@@ -1,5 +1,28 @@
 package parking.business;
 
 public class Vehicule {
+	String numeroImmatriculation;
+	String marque;
+	String modele;
+	String proprietaire;
+	
+	public Vehicule() {
+		
+	}
+	
+	public Vehicule(String numeroImmatriculation, String marque, String modele,
+			String proprietaire) {
+		this.numeroImmatriculation = numeroImmatriculation;
+		this.marque = marque;
+		this.modele = modele;
+		this.proprietaire = proprietaire;
+	}
 
+	@Override
+	public String toString() {
+		return "Vehicule [numeroImmatriculation=" + numeroImmatriculation
+				+ ", marque=" + marque + ", modele=" + modele
+				+ ", proprietaire=" + proprietaire + "]";
+	}
+		
 }
