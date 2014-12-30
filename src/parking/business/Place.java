4763575d6c65b319243f13af10dca173f8cc809a
Josua Gonzalez <jdpepi@gmail.com>
2014-12-30 15:19:58 +0100

    Gestion d'une place de parking

--- src/parking/business/Place.java
+++ src/parking/business/Place.java
@@ -1,5 +1,25 @@
 package parking.business;
 
 public class Place {
-
+	Vehicule vehiculeGare;
+	
+	public Place() {
+		vehiculeGare = null;
+	}
+	
+	public void parkVehicule(Vehicule v) throws PlaceOccupeeException {
+		if(!isFree()) throw new PlaceOccupeeException();
+		vehiculeGare = v;
+	}
+	
+	public Vehicule unparkVehicule() {
+		if(isFree()) throw new PlaceLibreException();
+		Vehicule unparked = vehiculeGare;
+		vehiculeGare = null;
+		return unparked;
+	}
+	
+	public boolean isFree() {
+		return vehiculeGare == null;			
+	}
 }
