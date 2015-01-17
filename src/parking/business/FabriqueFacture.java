e5a6e596ba403b43a0f1cf2e40dc66cbaf8143ee
Josua Gonzalez <jdpepi@gmail.com>
2015-01-17 17:10:03 +0100

    Doc

--- src/parking/business/FabriqueFacture.java
+++ src/parking/business/FabriqueFacture.java
@@ -1,13 +1,28 @@
 package parking.business;
 
+/**
+ * Fabrique de facture simple
+ * Implémente l'interface IFabriqueFacture
+ */
 public class FabriqueFacture implements IFabriqueFacture {
+	/**
+	 * Numéro de facture actuel, s'incrémente à chaque génération de facture
+	 */
 	private static int numFacture = 0;
+	
+	/**
+	 * Tarif à appliquer aux factures
+	 */
 	private static double tarif;
 	
-	public FabriqueFacture(double Tarif) {
+	/**
+	 * Construit une fabrique de factures avec un tarif spécifié
+	 * @param TarifHT Tarif hors taxe des factures
+	 */
+	public FabriqueFacture(double TarifHT) {
 		this.tarif = tarif;
 	}
-	
+
 	public Facture genererFacture() {
 		return new Facture(numFacture++, tarif);
 	}
