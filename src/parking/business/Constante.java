6a527a6a64727e8b85fd572e566201808365abd4
Josua Gonzalez <jdpepi@gmail.com>
2015-01-17 17:09:56 +0100

    Constante : dossier de sauvegarde des factures

--- src/parking/business/Constante.java
+++ src/parking/business/Constante.java
@@ -4,4 +4,5 @@ public class Constante {
 	public final static int NUMERO_PREMIERE_PLACE = 1;
 	public final static int NOMBRE_PLACES = 10;
 	public final static double TVA = 1.196;
+	public final static String DOSSIER_FACTURES = "factures";
 }
