8fb7e8e2b83671d5cc78b3b625ad6a066429f81c
Josua Gonzalez <jdpepi@gmail.com>
2015-01-17 16:06:58 +0100

    Constante : premier numéro de place. Permet de faire commencer le parking à la place 1.

--- src/parking/business/Constante.java
+++ src/parking/business/Constante.java
@@ -1,6 +1,7 @@
 package parking.business;
 
 public class Constante {
+	public final static int NUMERO_PREMIERE_PLACE = 1;
 	public final static int NOMBRE_PLACES = 10;
 	public final static double TVA = 1.196;
 }
