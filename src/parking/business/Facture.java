f7a0628c834473db64238ec750a28db0fd7dc149
Josua Gonzalez <jdpepi@gmail.com>
2015-01-17 17:10:14 +0100

    Doc + sauver facture dans un fichier

--- src/parking/business/Facture.java
+++ src/parking/business/Facture.java
@@ -1,14 +1,52 @@
 package parking.business;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Facture {
+	/**
+	 * Numéro de cette facture
+	 */
 	private int numFacture;
+	
+	/**
+	 * Tarif hors taxe
+	 */
 	private double tarifHT;
+	
+	/**
+	 * Tarif toutes taxes comprises
+	 */
 	private double tarifTTC;
 	
+	/**
+	 * Construit une facture
+	 * 
+	 * @param numFacture Le numéro de la facture
+	 * @param tarifHT Le tarif hors taxe
+	 */
 	public Facture(int numFacture, double tarifHT) {
 		this.numFacture = numFacture;
 		this.tarifHT = tarifHT;
 		this.tarifTTC = tarifHT * Constante.TVA; 
+	}
+	
+	public void sauverDansFichier(String nomFichier) {
+		File fichier = new File(Constante.DOSSIER_FACTURES+File.pathSeparator+nomFichier);
+		
+		try {
+			fichier.mkdirs();
+			PrintStream out = new PrintStream(fichier);
+			out.println(toString());		
+			out.close();
+		} catch (SecurityException e) {
+			System.out.println("Erreur : impossible de créer le dossier");
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			System.out.println("Erreur : impossible d'écrire dans le fichier.");
+			e.printStackTrace();
+		}
 	}
 	
 	public String toString() {
