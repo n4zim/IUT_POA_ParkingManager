16258b12554f83fa14994d76ca644262df9dbec0
Josua Gonzalez <jdpepi@gmail.com>
2015-01-17 17:10:37 +0100

    Doc

--- src/parking/business/IFabriqueFacture.java
+++ src/parking/business/IFabriqueFacture.java
@@ -1,5 +1,12 @@
 package parking.business;
 
+/**
+ * Interface pour une fabrique de factures
+ */
 public interface IFabriqueFacture {
+	/**
+	 * Génère une nouvelle facture
+	 * @return Facture généree
+	 */
 	public Facture genererFacture();
 }
