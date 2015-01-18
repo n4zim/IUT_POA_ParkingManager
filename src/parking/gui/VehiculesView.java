ebab4ac48595350746eed73c732a47c51f188175
CarolLai <otamaro.dia@gmail.com>
2015-01-18 15:20:05 +0100

    Ajout d'une fenetre de confirmation quand on clique sur quitter dans le menu

--- src/parking/gui/VehiculesView.java
+++ src/parking/gui/VehiculesView.java
@@ -43,18 +43,22 @@ public class VehiculesView extends JPanel {
 		add(scrollPane);
 	}
 	
+	private static void fenetreConfirmationFermer (){
+		String YesNo[] = {"Oui", "Non"};
+		int PromptResult = JOptionPane.showOptionDialog(null,
+				"Fermer le programme ?", "Marre des parkings ?",
+				JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+				null, YesNo, YesNo[1]);
+		if(PromptResult==JOptionPane.YES_OPTION) System.exit(0);
+	}
+	
 	static void afficher() {
-		JFrame frame = new JFrame("Véhicules");
+		JFrame frame = new JFrame("Parking");
 
 		frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		frame.addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent we) {
-				String YesNo[] = {"Oui", "Non"};
-				int PromptResult = JOptionPane.showOptionDialog(null,
-						"Fermer le programme ?", "Marre des parkings ?",
-						JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
-						null, YesNo, YesNo[1]);
-			  if(PromptResult==JOptionPane.YES_OPTION) System.exit(0);
+				fenetreConfirmationFermer ();
 			}
 		});
 		
@@ -63,12 +67,31 @@ public class VehiculesView extends JPanel {
 		JMenu printMenu = new JMenu("Imprimer");
         JMenu helpMenu = new JMenu("Aide");
         JMenu quitMenu = new JMenu("Quitter");
+        quitMenu.addMouseListener(new MouseListener() {
+        	
+			@Override
+			public void mouseReleased(MouseEvent e) {}
+			
+			@Override
+			public void mousePressed(MouseEvent e) {}
+			
+			@Override
+			public void mouseExited(MouseEvent e) {}
+			
+			@Override
+			public void mouseEntered(MouseEvent e) {}
+			
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				fenetreConfirmationFermer ();
+			}
+		});
         menuBar.add(printMenu);
         menuBar.add(helpMenu);
         menuBar.add(quitMenu);
         JMenuItem ProgramHelp = new JMenuItem("Documentation");
         helpMenu.add(ProgramHelp);
-        JMenuItem ProgramAbout = new JMenuItem("A propos...");
+        JMenuItem ProgramAbout = new JMenuItem("A propos");
         helpMenu.add(ProgramAbout);
 		
 		VehiculesView newContentPane = new VehiculesView();
