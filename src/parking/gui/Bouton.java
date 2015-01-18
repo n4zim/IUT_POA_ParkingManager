a11da85bb606076165ea1e5f854dc01eb56fcc70
Nazim Lachter <lachtater@gmail.com>
2015-01-18 21:30:52 +0100

    Clic droit

--- src/parking/gui/Bouton.java
+++ src/parking/gui/Bouton.java
@@ -2,8 +2,7 @@ package parking.gui;
 
 import java.awt.Color;
 import java.awt.Component;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 import java.io.Externalizable;
 
 import javax.swing.JButton;
@@ -68,12 +67,24 @@ public class Bouton extends JButton {
         setHorizontalTextPosition(SwingConstants.CENTER);
         
         initPopupMenu();
-        
-		addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				popup.show((Component)e.getSource(), (getX() + 25), (getY() + 25));
+
+		addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				showPopup(e);
+			}
+
+			public void mouseReleased(MouseEvent e) {
+				showPopup(e);
+			}
+
+			private void showPopup(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					popup.show((Component) e.getSource(), (getX() + 25),
+							(getY() + 25));
+				}
 			}
 		});
+
     }
     
     /**
