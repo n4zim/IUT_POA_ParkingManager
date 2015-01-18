a708583a9ece57094d27af24797f78aa5d2a0168
Nazim Lachter <lachtater@gmail.com>
2015-01-18 17:08:17 +0100

    Menu trop bien (écrasé par le méchant merge...)

--- src/parking/gui/ParkingView.java
+++ src/parking/gui/ParkingView.java
@@ -5,19 +5,35 @@ import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 //import java.awt.Toolkit;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
 
 public class ParkingView extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 
+	final JPopupMenu popup = new JPopupMenu();
+
 	ParkingView() {
 		super("Etat du parking");
+
+		JMenuItem menuLiberer = new JMenuItem("Libérer");
+		popup.add(menuLiberer);
+		JMenuItem menuReserver = new JMenuItem("Réserver");
+		popup.add(menuReserver);
+		JMenuItem menuOccuper = new JMenuItem("Occuper");
+		popup.add(menuOccuper);
+
 		setUndecorated(true);
 		Container contenu = getContentPane();
 
@@ -49,6 +65,30 @@ public class ParkingView extends JFrame {
 				place.setText(Integer.toString(i));
 				place.setPreferredSize(new Dimension(50, 50));
 				grille.add(place);
+
+				
+				menuLiberer.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("Libérée");
+					}
+				});
+				menuReserver.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("Réservée");
+					}
+				});
+				menuOccuper.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						System.out.println("Occupée");
+					}
+				});
+				
+				place.addActionListener(new ActionListener() {
+		            public void actionPerformed(ActionEvent e) {
+		            	popup.show(place, (getX() - 25), (getY() - 25));
+		            }
+				});
+				
 				JPanel panneau = new JPanel();
 				panneau.add(place);
 				grille.add(panneau);
@@ -60,11 +100,13 @@ public class ParkingView extends JFrame {
 		contenu.add(bouton);
 
 		pack();
-		/*setLocation((Toolkit.getDefaultToolkit().getScreenSize().width+600)/3,
-				(Toolkit.getDefaultToolkit().getScreenSize().height-600)/2);*/
-		//setLocation(800, 200);
+		/*
+		 * setLocation((Toolkit.getDefaultToolkit().getScreenSize().width+600)/3,
+		 * (Toolkit.getDefaultToolkit().getScreenSize().height-600)/2);
+		 */
+		// setLocation(800, 200);
 		setLocation(50, 50);
-		
+
 		setVisible(true);
 	}
 
